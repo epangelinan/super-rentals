20a72e25bddf8fcea7a872cda545069f31161299
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-06 22:58:08 -0700

    add code for Dynamic routing Find Record

--- app/router.js
+++ app/router.js
@@ -6,9 +6,11 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL
 });
 
+//Now, when the router receives a request matching /rental/:rental_id (such as from a link within our application that contains the rental's id) it will map the URL to the rental route handler, rental.js, and send a params hash that includes the value of the :rental_id segment in the URL.
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('rental', {path: '/rental/:rental_id'});
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,33 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
-  },
-
-  actions: {
+     return this.store.findAll('rental');
+   },
+   actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-
- //For each key in the params,
-// if it is NOT undefined,
-// take the rental and set the property that matches the current key, to the value of the current key,
-// after looping through all of the keys, save the rental,
-// transition to the index route.
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-
-    destroyRental(rental) {
-      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
