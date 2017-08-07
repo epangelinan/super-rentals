6d72e6e4aafee78279b97867d10244241e3faf9e
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-06 21:44:18 -0700

    add code for Components-Update section

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,21 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
+ //For each key in the params,
+// if it is NOT undefined,
+// take the rental and set the property that matches the current key, to the value of the current key,
+// after looping through all of the keys, save the rental,
+// transition to the index route.
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
